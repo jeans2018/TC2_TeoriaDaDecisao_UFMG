d3d8bbe5990ab4384dd49a0c5d373745fc99c80c
Jean Santos <jean@engenhariadesistemas.com>
2018-11-26 18:47:19 -0200

    Adicionados resultados do TC1 para criar matriz multi-critério

--- DEA problem/main.m
+++ DEA problem/main.m
@@ -6,21 +6,30 @@ N  = 10; % number of solutions
 n  = 6;  % number of decision variables
 lb = [0.05 0.05 0.05 0.05 0.05 0.05];  % lower bound
 ub = [0.5  0.6  1.0  1.2  1.0  0.6];   % upper bound
+nc = 50; %number of cells
+nit = 80;
 X  = (repmat(lb(:),1,N) + repmat((ub(:)-lb(:)),1,N).*rand(n,N));
 
 % Evaluation of the candidate solutions
 fX = dea(X);
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% optimization algorithm (done)
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% optimization algorithm
+cell_x = EvDif(X,lb,ub,nc,nit);
+j=[];
+f=[];
+
+for g=1:nc
+   [fp,jP] = nondominatedpoints(cell_x{g},dea(cell_x{g}));
+   j = [j jP];
+   f = [f fp];
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 % Eliminate Pareto dominated solutions
-[X,fX] = nondominatedpoints(X,fX);
+[X,fX] = nondominatedpoints(f,j);
 
 
 save solutions X fX
